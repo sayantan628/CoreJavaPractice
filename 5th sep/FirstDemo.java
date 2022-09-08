1751a1ecb13e906bc05031d373a19c1edbc5533b
sayantan628 <113085544+sayantan628@users.noreply.github.com>
2022-09-09 00:59:14 +0530

    Add files via upload

---
+++ 5th sep/FirstDemo.java
@@ -0,0 +1,18 @@
+/*
+@class name: First Demo
+@author: sayantan naskar
+@date: 5th sep 2022
+*/
+//declaring class 
+
+
+class FirstDemo
+{
+	// calling main method               
+public static void main(String args[])
+{
+System.out.println("hello god"); // print 
+}
+//end of main 
+}
+//end of class 
